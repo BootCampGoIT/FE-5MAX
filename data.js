b08b8d59d9a6ab563f312a8ad0c018a2d63b326c
ZYAT1488228 <37843744+ZYAT1488228@users.noreply.github.com>
2020-06-06 10:02:25 +0300

    Project

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const system = [
         category: "system",
         title: "OVNS W01",
         url: "https://dartvaper.com.ua/files/products/w91.534x434.jpg",
-        description: "",
+        description: "Нет ничего невозможного на рынке стартовых наборов, даже подвинуть с трона Juul можно, если только захотеть. OVNS захотели, и если не подвинули «короля китов», то серьёзно пошатнули его монополию в новичковой нише. OVNS W01 представлен в пяти расцветках, у него металлический корпус, и длинный зарядный шнур в комплекте.",
         price: 329
     },
     {
@@ -12,7 +12,7 @@ export const system = [
         category: "system",
         title: "JUUL",
         url: "https://dartvaper.com.ua/files/products/juul-basic---silver-limited-edition.534x434.jpg",
-        description: "",
+        description: "Ещё один взгляд на то, каким может быть идеальный стартовый набор для новичков, и всех, кто ещё нуждается в никотиновых «перекусах». JUUL Basic - Silver Limited Edition — это улучшенная в плане производительности и автономности версия стандартного JUUL, в новом ярком серебряном дизайне.",
         price: 759
     },
     {
@@ -20,7 +20,7 @@ export const system = [
         category: "system",
         title: "MYLE DEVICE (ORIGINAL)",
         url: "https://dartvaper.com.ua/files/products/hot_red_device-500x500_17fb8467-8223-44dc-96b3-aea0e8bad4cf.534x434.jpg",
-        description: "",
+        description: "Слава Juul не даёт спокойно спать производителям, как топовым брендам, так и новеньким, все, как один, они пускаются вдогонку, и у некоторых, например MYLE, получается не только догнать, но и в некоторых отношениях перегнать популярного пододела. Стартовый набор MYLE Device идеально подходит для бросающих курить, он оснащён встроенной батареей на ≈240 тяг, и, в зависимости от версии, может комплектоваться четырьмя картриджами.",
         price: 750
     },
     {
@@ -28,7 +28,7 @@ export const system = [
     category: "system",
     title: "MYLE DEVICE (ORIGINAL)",
     url: "https://dartvaper.com.ua/files/products/ovns2saberblue.534x434.jpg",
-    description: "",
+    description: "The show must go on! Линейка Saber продолжает пополняться топовыми девайсами, встречайте OVNS Saber 2 — под-система с зарядным портом Type-C (кабель в комплекте), встроенным аккумулятором 400 (или 600, в зависимости от версии) мАч, и двумя многоразовыми картриджами в наборе.",
     price: 469
 },
 {
@@ -36,9 +36,34 @@ export const system = [
     category: "system",
     title: "СТАРТОВЫЙ НАБОР OVNS JC01 PRO POD KIT",
     url: "https://dartvaper.com.ua/files/products/startovyj-nabor-ovns-jc01-pro-pod-kit-original4.534x434.jpg",
-    description: "",
-    price: 469
+    description: "Усовершенствованная версия оригинального JC01, под стал более функциональным, теперь он поддерживает регулировку напряжения, а картридж на 0,3 мл вместительней. JC01 Pro комплектуется картриджем с керамическим испарителем с сопротивлением 1,5 Ом, идеально подходящим для никотиновых жижек. Подходят картриджи для JUUL и OVNS W01",
+    price: 369 
 },
+{
+    id: "6",
+    category: "system",
+    title: "СТАРТОВЫЙ НАБОР OVNS SABER S POD SYSTEM",
+    url: "https://dartvaper.com.ua/files/products/ovns-saber-s-pod-sistemyi-pod-system-chernyij.534x434.jpg",
+    description: "OVNS Saber S Pod System Starter Kit - электронная сигарета от производителя OVNS с компактным корпусом из нержавеющей стали и пластика,  небольшими габаритами и является отличным выбором для новичков в сфере парения, так как имеет все необходимые параметры и функции в своем ценовом сегменте. Отличие от своего собрата OVNS Saber Pod System отсутствием LED подсветке сбоку.",
+    price: 449
+},
+{
+    id: "7",
+    category: "system",
+    title: "СТАРТОВЫЙ НАБОР ELEAF ELVEN POD STARTER KIT",
+    url: "https://dartvaper.com.ua/files/products/eleaf_elven_pod_system_-_front_view.534x434.jpg",
+    description: "Eleaf Elven Pod Starter Kit - новая стильная под система имеющая форму ручки, со встроенным аккумулятором на 360 мАч обеспечивающая достаточное количество энергии для парения вашей любимой жидкости, сам картридж имеет объем 1,6 мл с сопротивлением на 1,6 Ом.",
+    price: 479 
+},
+{
+    id: "8",
+    category: "system",
+    title: "СТАРТОВЫЙ НАБОР ASMODUS FLOW POD KIT",
+    url: "https://dartvaper.com.ua/files/products/asmodus_flow_ultra_portable_pod_system_red_1.534x434.jpg",
+    description: "Asmodus Flow Pod Kit - относится к ультра портативным девайсам, поэтому его габаритные размеры приятно удивят многих – он чуть больше обычной шариковой ручки. ",
+    price: 499
+},
+
     
 ];
 
@@ -66,6 +91,62 @@ export const nikotine = [
         url: "https://dartvaper.com.ua/files/products/zhidkost-alchemist---cranapple-10-ml2.534x434.jpg",
         description: "Жидкость для pod system Alchemist - CranApple - микс сладкого яблока и терпкой клюквы, всегда взбодрит и придаст свежести на день.",
         price: 130
+    },
+    {
+        id: "4",
+        category: "nikotine",
+        title: "ЖИДКОСТЬ ALCHEMIST - FRAPPUCCINO, 10 МЛ",
+        url: "https://dartvaper.com.ua/files/products/alchemist-frappuccino.534x434.jpg",
+        description: "Жидкость для podsystem Alchemist - Frappuccino - насыщенный вкус кофе, которого сейчас так не хватает",
+        price: 130
+    },
+    {
+        id: "5",
+        category: "nikotine",
+        title: "ЖИДКОСТЬ ALCHEMIST - ICEBERG MANGO, 10 МЛ",
+        url: "https://dartvaper.com.ua/files/products/alchemist-iceberg-mango.534x434.jpg",
+        description: "Жидкость для podsystem Alchemist - Iceberg Mango - насыщенный аромат спелого сладкого манго с освежающим холодком.",
+        price: 130
+    },
+    {
+        id: "6",
+        category: "nikotine",
+        title: "ЖИДКОСТЬ ALCHEMIST - PITAYA PEACH, 10 МЛ",
+        url: "https://dartvaper.com.ua/files/products/alchemist-pitaya-peach.534x434.jpg",
+        description: "Жидкость для podsystem Alchemist - Pitaya Peach - идеальное сочетание экзотического драгонфрута и спелых сочных персиков.",
+        price: 130
+    },
+    {
+        id: "7",
+        category: "nikotine",
+        title: "ЖИДКОСТЬ ALCHEMIST - RICHAPPLE, 10 МЛ",
+        url: "https://dartvaper.com.ua/files/products/zhidkost-alchemist---richapple-10-ml.534x434.jpg",
+        description: "Жидкость для podsystem Alchemist - RichApple это вкус освежающего хрустящего, красного яблока, доведенного до сладкого нектара, в сочетании с примесью солевого никотина наилучшего качества.",
+        price: 130
+    },
+    {
+        id: "8",
+        category: "nikotine",
+        title: "ЖИДКОСТЬ ALCHEMIST - VERO TOBACCO, 10 МЛ",
+        url: "https://dartvaper.com.ua/files/products/zhidkost-alchemist---vero-tobacco-10-ml.534x434.jpg",
+        description: "Жидкость для podsystem Alchemist - Vero Tobacco вкус настоящих, пепельных сигарет для тех, кто ценит классику.",
+        price: 130
+    },
+    {
+        id: "9",
+        category: "nikotine",
+        title: "ЖИДКОСТЬ ALCHEMIST - VANILLA TOBACCO, 10 МЛ",
+        url: "https://dartvaper.com.ua/files/products/zhidkost-alchemist---vanilla-tobacco-10-ml2.534x434.jpg",
+        description: "Жидкость для pod system Alchemist - Vanilla Tobacco - вкус изысканного, легкого табака дополняется прекрасным ароматом ванили и незабываемыми ощущениями.",
+        price: 130
+    },
+    {
+        id: "10",
+        category: "nikotine",
+        title: "ЖИДКОСТЬ ALCHEMIST - RASP BASIL, 10 МЛ",
+        url: "https://dartvaper.com.ua/files/products/zhidkost-alchemist---rasp-basil-10-ml2.534x434.jpg",
+        description: "Жидкость для pod system Alchemist - Rasp Basil - кисло-сладкая малина, дополненная легким оттенком базилика, лучшее сочетание для жидкости на каждый день.",
+        price: 130
     }
 ]
 
@@ -73,25 +154,33 @@ export const cartridge = [
     {
         id: "1",
         category: "cartridge",
-        title: "My Little Cloud",
+        title: "СМЕННЫЙ ИСПАРИТЕЛЬ OVNS W01 CARTRIDGE (ORIGINAL) 1.8 ОМ",
         url: "https://dartvaper.com.ua/files/products/ovns-w01.534x434.jpg",
-        description: "My Little Cloud is an ASMR application but like none available on the market today. Enjoy thunder and rain sounds recorded by Emmy-winner and nature recording genius Gordon Hempton, while watching fluid animations on your computer screen.",
-        price: 360
+        description: "Сменный перезаправляемый картридж для стартового набора OVNS W01 Pod Kit. Совместим с Juul и OVNS JC01",
+        price: 80
     },
     {
         id: "2",
         category: "cartridge",
-        title: "My Little Cloud",
-        url: "https://dartvaper.com.ua/files/products/ovns-w01.534x434.jpg",
-        description: "My Little Cloud is an ASMR application but like none available on the market today. Enjoy thunder and rain sounds recorded by Emmy-winner and nature recording genius Gordon Hempton, while watching fluid animations on your computer screen.",
-        price: 360
+        title: "СМЕННЫЙ КАРТРИДЖ A-VAPE ДЛЯ JUUL (ORIGINAL)",
+        url: "https://dartvaper.com.ua/files/products/a-vape-juul.534x434.jpg",
+        description: "Сменный перезаправляемый картридж для стартового набора Juul. Совместим с Juul, OVNS JC01 и OVNS W01",
+        price: 70
     },
     {
         id: "3",
         category: "cartridge",
-        title: "My Little Cloud",
-        url: "https://dartvaper.com.ua/files/products/ovns-w01.534x434.jpg",
-        description: "My Little Cloud is an ASMR application but like none available on the market today. Enjoy thunder and rain sounds recorded by Emmy-winner and nature recording genius Gordon Hempton, while watching fluid animations on your computer screen.",
-        price: 360
+        title: "СМЕННЫЙ КАРТРИДЖ OVNS SABER 2 (ORIGINAL) 1.4 ОМ",
+        url: "https://dartvaper.com.ua/files/products/ovns2sabercartridge_1.534x434.jpg",
+        description: "Сменный картридж для Pod системы OVNS Saber 2 Pod Kit.",
+        price: 89
+    },
+    {
+        id: "4",
+        category: "cartridge",
+        title: "СМЕННЫЙ КАРТРИДЖ OVNS SABER CARTRIDGE 1.8",
+        url: "https://dartvaper.com.ua/files/products/saber.534x434.jpg",
+        description: "Сменный картридж для стартового набора OVNS Saber Pod System Starter Kit.",
+        price: 65
     }
 ]
